84e9911bbb4d81a3bf811fe4d6557bfd4516461b
gonzaz92 <gonza.zorro92@gmail.com>
2024-12-29 02:05:58 -0300

    build

--- build.sh
+++ build.sh
@@ -10,3 +10,10 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    user = User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')
+"
