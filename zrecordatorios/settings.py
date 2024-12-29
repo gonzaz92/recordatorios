96a8ec55090032389b832ace6c4af58c7a91e2ec
gonzaz92 <gonza.zorro92@gmail.com>
2024-12-29 02:02:18 -0300

    Volvemos a postgree

--- zrecordatorios/settings.py
+++ zrecordatorios/settings.py
@@ -85,19 +85,19 @@ WSGI_APPLICATION = 'zrecordatorios.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# DATABASES= {
-#     'default': dj_database_url.config(
-#         default='postgresql://postgres:postgres@localhost:postgres',
-#         conn_max_age=600
-#     )
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES= {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:postgres',
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
